db15c9648a8114d92b11828a2734d082373db0c0
tafryn <tafryn@gmail.com>
2018-04-01 13:37:24 -0700

    Add lock timer script that uses xidlehook to engage screen locker.

---
+++ lock-timer.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo "No timer command supplied. Exiting..."
+    exit
+fi
+
+dir="$(dirname "$0")"
+
+rm /tmp/xidlehook.sock
+
+read -r -d '' lock << EOF
+    # Undim screen
+    xrandr --output "\$(xrandr | grep primary | cut -d ' ' -f 1)" --brightness 1
+
+    # Pause notifications
+    pkill dunst -USR1
+
+    $1
+
+    # Start notifications
+    pkill dunst -USR2
+EOF
+
+xidlehook \
+  --time 15 \
+  --timer "$lock" \
+  --notify 10 \
+  --notifier  'xrandr --output "$(xrandr | grep primary | cut -d " " -f 1)" --brightness .1' \
+  --canceller 'xrandr --output "$(xrandr | grep primary | cut -d " " -f 1)" --brightness 1' \
+  --not-when-fullscreen \
+  --not-when-audio \
+  --socket /tmp/xidlehook.sock
