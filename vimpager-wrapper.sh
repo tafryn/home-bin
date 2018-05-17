a9b6ea76c8c892b52d05b57677f6116b3773f146
tafryn <tafryn@gmail.com>
2018-05-17 16:00:22 -0700

    Use bare bones config file for vimcat in wrapper script.

--- vimpager-wrapper.sh
+++ vimpager-wrapper.sh
@@ -15,7 +15,7 @@ TERM_LINES=$(tput lines)
 INPUT_LINES=$(echo -n "$INPUT" | wc -l | cut -d' ' -f1)
 
 if [ "$TERM_LINES" -gt "$INPUT_LINES" ]; then
-    echo -n "$INPUT" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | vimcat
+    echo -n "$INPUT" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | vimcat -u ~/.vimpagerrc
 else
     echo -n "$INPUT" | vimpager
 fi
