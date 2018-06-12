518a0c4355bbb760b690af4bd4fa6e0de0a04424
tafryn <tafryn@gmail.com>
2018-06-12 11:18:19 -0700

    Correct vim wrapper script handling of filenames with spaces.

--- vimpager-wrapper.sh
+++ vimpager-wrapper.sh
@@ -5,7 +5,7 @@
 # if paging is not needed.
 
 if [ $# -ge 1 -a -f "$1" ]; then
-    INPUT=$(cat $1)
+    INPUT=$(cat "$@")
 else
     INPUT=$(cat "-")
 fi
@@ -15,7 +15,7 @@ TERM_LINES=$(tput lines)
 INPUT_LINES=$(echo -n "$INPUT" | wc -l | cut -d' ' -f1)
 
 if [ "$TERM_LINES" -gt "$INPUT_LINES" ]; then
-    echo -n "$INPUT" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | vimcat -u ~/.vimpagerrc
+    echo -n "$INPUT" | sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" | vimcat
 else
     echo -n "$INPUT" | vimpager
 fi
