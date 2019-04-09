cc28bdfdad2be14389939b55efcd707e5c0fd891
tafryn <tafryn@gmail.com>
2019-04-09 16:32:36 -0700

    Remove debug lines from nested_navigate script.

--- nested_navigate.sh
+++ nested_navigate.sh
@@ -59,14 +59,10 @@ pane_at_edge() {
 
   cmd="#{pane_id}:#{pane_$coord}:#{?pane_active,_active_,_no_}"
   panes=$(tmux list-panes -F "$cmd")
-  # echo "$panes" >> /tmp/nested_navigate_log
   active_pane=$(echo "$panes" | grep _active_)
   # active_pane_id=$(echo "$active_pane" | cut -d: -f1)
   active_coord=$(echo "$active_pane" | cut -d: -f2)
   coords=$(echo "$panes" | cut -d: -f2)
-
-  # echo "active_coord: $active_coord" >> /tmp/nested_navigate_log
-  # echo "coords: $coords" >> /tmp/nested_navigate_log
 
   if [ "$op" == ">=" ]; then
     test_coord=$(echo "$coords" | sort -nr | head -n1)
@@ -120,14 +116,6 @@ shift $((OPTIND-1))
 
 contains_element "$1" "${NAVIGATION_DIRECTIONS[@]}" || exit 1
 
-if ! $VIM_CALL; then
-    echo "------------------------------" >> /tmp/nested_navigate_log
-    echo "$0" "$@" >> /tmp/nested_navigate_log
-else
-    echo "$0" "-V" "$@" >> /tmp/nested_navigate_log
-fi
-
-
 DIRECTION="$1"
 
 # Check if terminal is focused
@@ -136,9 +124,6 @@ FOCUSED_CLASS_NAMES=$(xprop -id "$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | c
 FOCUSED_TITLE=$(xprop -id "$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)" _NET_WM_NAME | grep -o '".*"' | tr -d '",')
 
 FOCUSED_PID=$(xprop -id "$(xprop -root 32x '\t$0' _NET_ACTIVE_WINDOW | cut -f 2)" _NET_WM_PID | grep -o '[[:digit:]]\+')
-
-# echo "$FOCUSED_CLASS_NAMES" >> /tmp/nested_navigate_log
-echo "$FOCUSED_PID" >> /tmp/nested_navigate_log
 
 for class_name in $FOCUSED_CLASS_NAMES; do
     if contains_element "$class_name" "${TERMINAL_CLASS_NAMES[@]}"; then
@@ -151,57 +136,40 @@ TMUX_IN_FOCUSED_TERMINAL=false
 TYPE=i3
 
 if $TERMINAL_FOCUSED; then
-    echo "A terminal is focused" >> /tmp/nested_navigate_log
-
     for child in $(descendents_of "$FOCUSED_PID"); do
         if has_child "$child" "tmux"; then
             TMUX_IN_FOCUSED_TERMINAL=true
-            echo "TMUX in focused terminal" >> /tmp/nested_navigate_log
 
             if pane_at_edge "$DIRECTION"; then
-                echo "Pane Edge" >> /tmp/nested_navigate_log
                 TYPE=i3
             else
-                echo "Inner Pane " >> /tmp/nested_navigate_log
                 TYPE=tmux
             fi
         fi
     done
 
-    # echo "$FOCUSED_TITLE" >> /tmp/nested_navigate_log
 	if echo "$FOCUSED_TITLE" | grep -q VIM && ! $VIM_CALL; then
-        echo "Sending command to VIM" >> /tmp/nested_navigate_log
         TYPE=vim
 	fi
 else
-    echo "Focused window isn't a terminal" >> /tmp/nested_navigate_log
 	TYPE=i3
 fi
 
 contains_element "$TYPE" "${NAVIGATION_TYPES[@]}" || exit 1
 
-echo -n "$TYPE " >> /tmp/nested_navigate_log
 
 case "$TYPE" in
 i3)
-    echo "${LONG_NAVIGATION_DIRECTIONS[$(index_of "$DIRECTION" "${NAVIGATION_DIRECTIONS[@]}")]}" >> /tmp/nested_navigate_log
     i3-msg -t run_command focus "${LONG_NAVIGATION_DIRECTIONS[$(index_of "$DIRECTION" "${NAVIGATION_DIRECTIONS[@]}")]}" >/dev/null
 	;;
 tmux)
-    echo "$DIRECTION" | tr 'LDUR' 'LDUR' >> /tmp/nested_navigate_log
     tmux select-pane -"$(echo "$DIRECTION" | tr 'LDUR' 'LDUR')"
 	;;
 vim)
-    echo "$DIRECTION" | tr 'LDUR' 'hjkl' >> /tmp/nested_navigate_log
     if $TMUX_IN_FOCUSED_TERMINAL; then
-        echo "Sent command to vim with tmux" >> /tmp/nested_navigate_log
         tmux send-keys C-"$(echo "$DIRECTION" | tr 'LDUR' 'hjkl')"
     else
-        echo -n "Sent command to vim with xdotool " >> /tmp/nested_navigate_log
-        echo -n "$(echo "$DIRECTION" | tr 'LDUR' 'hjkl')" >> /tmp/nested_navigate_log
         xdotool key --clearmodifiers --window \""$(xdotool getactivewindow)"\" ctrl+"$(echo "$DIRECTION" | tr 'LDUR' 'hjkl')"
     fi
 	;;
 esac
-
-echo " " >> /tmp/nested_navigate_log
