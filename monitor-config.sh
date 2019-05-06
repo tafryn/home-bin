c483950bcf7997bc6a7f570daeb6decca0aff2c6
tafryn <tafryn@gmail.com>
2019-05-06 12:05:37 -0700

    Adjust monitor config script for new setup.

--- monitor-config.sh
+++ monitor-config.sh
@@ -4,10 +4,17 @@ LAPTOP=DP-2
 LEFT_MON=DP-0.2
 CENTER_MON=DP-0.1
 RIGHT_MON=DP-0.3
+
 #xrandr --output $CENT_MON --primary --rotate left
 #xrandr --output $RIGHT_MON --rotate left --right-of $CENT_MON
 #xrandr --output $LEFT_MON --rotate left --left-of $CENT_MON
 #xrandr --output $LAPTOP --left-of $LEFT_MON
 #xrandr --output $LAPTOP --pos 0x800
 #xrandr --output $CENT_MON --primary --rotate left --output $RIGHT_MON --rotate left --right-of DP-3 --output $LEFT_MON --rotate left --left-of DP-3 --output $LAPTOP --pos 0x800
-xrandr --output "$LAPTOP" --primary --pos 0x340 --output "$LEFT_MON" --rotate left --pos 1920x0 --output "$CENTER_MON" --rotate left --pos 3000x0 --output "$RIGHT_MON" --rotate right --pos 4080x0 
+
+xrandr --output "$LEFT_MON" --off
+xrandr --output "$CENTER_MON" --off
+xrandr --output "$RIGHT_MON" --off
+xrandr --output "$LAPTOP" --fb 1920x1080
+xrandr --output "$LAPTOP" --panning "0x0+0+0/0x0+0+0/0/0/0/0"
+xrandr --output "$LAPTOP" --primary --pos 0x0 --mode 1920x1080 --output "$LEFT_MON" --pos 1920x0 --mode 1920x1080 --rotate left --output "$CENTER_MON" --pos 3000x0 --mode 1920x1080 --rotate left --output "$RIGHT_MON" --pos 4080x0 --mode 1920x1200 --rotate right 
